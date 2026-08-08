b4de34c76f2b13ddc984e5c04aa971de2b8ef34a
sricharankrishnan <sricharan.krishnan@gmail.com>
2026-08-08 09:26:07 +0530

    [feat] Get Random Verse
    - add new functionality get random verse
    - randomness determined by optional prop, with translation-id and book-id
    - final api-url is determined by incoming props
    - fallback api-url is web based translated verse
    - updated root barrel export file

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export * from "./get-available-translations/index.js";
 export * from "./get-list-of-book-ids/index.js";
 export * from "./get-random-verse-old-testament/index.js";
 export * from "./get-random-verse-new-testament/index.js";
+export * from "./get-random-verse/index.js";
